61c524a1b153de4d21b76b320ca616022c05c960
HedBit <itshedbit@gmail.com>
2024-09-19 09:41:30 +0330

    Feat: Update token to ERC20 basic standard structure

--- Fungible Token Standard/app.py
+++ Fungible Token Standard/app.py
@@ -12,14 +12,45 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 db = SQLAlchemy(app)
 zsequencer_url = 'http://localhost:8323/node/transactions'
 
-# Token balance model
+TOKEN_NAME = "ZellularToken"
+TOKEN_SYMBOL = "ZTK"
+TOKEN_DECIMALS = 18
+TOTAL_SUPPLY = 1000000000 * (10 ** TOKEN_DECIMALS)  # 1 billion tokens
+
+# Token balance model using int to represent whole tokens in smallest unit (like "wei")
 class Balance(db.Model):
     public_key = db.Column(db.String(500), primary_key=True)
-    amount = db.Column(db.Float, nullable=False, default=10)  # Default to 10 tokens for each user
+    amount = db.Column(db.Integer, nullable=False)
 
 @app.before_first_request
 def create_tables():
     db.create_all()
+    # Initialize one address with 1 billion tokens
+    genesis_address = "your_genesis_public_key_base64"  # Replace with actual base64-encoded public key
+    if not Balance.query.filter_by(public_key=genesis_address).first():
+        genesis_balance = Balance(public_key=genesis_address, amount=TOTAL_SUPPLY)
+        db.session.add(genesis_balance)
+        db.session.commit()
+
+# ERC-20 like token details
+@app.route('/token_info', methods=['GET'])
+def token_info():
+    return jsonify({
+        "name": TOKEN_NAME,
+        "symbol": TOKEN_SYMBOL,
+        "total_supply": TOTAL_SUPPLY,
+        "decimals": TOKEN_DECIMALS
+    })
+
+# Retrieve balance of an address
+@app.route('/balanceOf', methods=['GET'])
+def balance_of():
+    public_key = request.args.get('public_key')
+    balance = Balance.query.filter_by(public_key=public_key).first()
+    if balance:
+        return jsonify({"balance": balance.amount})
+    else:
+        return jsonify({"balance": 0})
 
 # Function to verify signatures
 def verify_transaction(transaction):
@@ -42,7 +73,7 @@ def transfer_tokens():
     
     sender_public_key = request.form['public_key']
     recipient_public_key = request.form['recipient']
-    amount = float(request.form['amount'])
+    amount = int(request.form['amount'])  # Using int to represent whole tokens (smallest unit like "wei")
     
     sender_balance = Balance.query.filter_by(public_key=sender_public_key).first()
     recipient_balance = Balance.query.filter_by(public_key=recipient_public_key).first()
@@ -56,8 +87,12 @@ def transfer_tokens():
         'timestamp': int(time.time())
     }
     headers = {"Content-Type": "application/json"}
-    requests.put(zsequencer_url, json=data, headers=headers)
+    zresponse = requests.put(zsequencer_url, json=data, headers=headers)
 
+    # Verify the response from Zellular
+    if zresponse.status_code != 200:
+        return jsonify({"message": "Error submitting transaction to Zellular"}), 500
+    
     return {'success': True}
 
 # Process finalized transactions from the Zellular sequencer
@@ -81,7 +116,7 @@ def process_loop():
 def process_transfer(transaction):
     sender_public_key = transaction['public_key']
     recipient_public_key = transaction['recipient']
-    amount = float(transaction['amount'])
+    amount = int(transaction['amount'])
 
     sender_balance = Balance.query.filter_by(public_key=sender_public_key).first()
     recipient_balance = Balance.query.filter_by(public_key=recipient_public_key).first()
@@ -103,3 +138,4 @@ def process_transfer(transaction):
 if __name__ == '__main__':
     Thread(target=process_loop).start()
     app.run(debug=True)
+
